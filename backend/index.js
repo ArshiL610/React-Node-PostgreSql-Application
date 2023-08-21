858295322b8c8156272068dd5bff3238b33e8608
ArshiL <arshilakkala@gmail.com>
2023-08-21 19:09:56 +0530

    commit 21 Aug 23'

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ pool.connect((err, client, release) => {
 })
   
 //get all users
-app.get('/get/users', (req, res, next) => {
+app.get('/get/users/all', (req, res, next) => {
     console.log("TEST DATA :");
     pool.query('Select * from users')
         .then(testData => {
@@ -64,7 +64,7 @@ app.get('/get/users/byId',(req,res,next) => {
     })
 })
 
-//get user data by name or username
+//get user data by name or username (for filter feature)
 app.get('/get/users/byName',(req,res,next) => {
     const Username = req.query.username;
     console.log("Fetching user data for name:", Username);
@@ -84,6 +84,26 @@ app.get('/get/users/byName',(req,res,next) => {
         res.status(500).send("Internal Server Error");
     })
 });
+
+//get user data by dept name (for filter feature)
+app.get('/get/users/byDept',(req,res) => {
+    const Dept = req.query.dept;
+    console.log("Fetching user data for the dept : ", Dept);
+    pool.query('Select * from users where dept = $1', [Dept])
+    .then(userData => {
+        if(userData.rows.length === 0){
+            res.status(404).send("user not found");
+        }
+        else{
+            res.send(userData.rows);
+            console.log('Successfully fetched user data')
+        }
+    })
+    .catch(error => {
+        console.log("Error fetching the client data : ", error);
+        res.status(500).send("Internal server error");
+    })
+})
 
 app.listen(port, () => {
   console.log(`React-Node App is running on port ${port}.`);
--- frontend/src/components/RenderTable.jsx
+++ frontend/src/components/RenderTable.jsx
@@ -8,6 +8,12 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import IconButton from '@mui/material/IconButton';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import FilterAltRoundedIcon from '@mui/icons-material/FilterAltRounded';
+
+
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -34,43 +40,103 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 const RenderTable = () => {
 
   const [userData, setUserData] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [selectedFilter, setSelectedFilter] = useState('');
+  const [filterVisible, setFilterVisible] = useState(false);
 
   useEffect(() => {
-    axios.get(`http://localhost:5000/get/users`)
+    axios.get(`http://localhost:5000/get/users/all`)
     .then(response => {
       setUserData(response.data);
     })
     .catch(error => {
       console.error('Error fetching data through frontend..', error);
     })
-  },[])
 
+    axios.get(`http://localhost:5000/get/users/byDept?dept=${selectedFilter}`)
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data through frontend..', error);
+      });
+
+  },[selectedFilter])
+
+  const handleSort = () => {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  }
+  const sortedData = [...userData].sort((a,b) => {
+    if(sortOrder === 'asc'){
+      return a.id - b.id;
+    }
+    else{
+      return b.id - a.id;
+    }
+  })
+
+  const handleFilterIconClick = () => {
+    setFilterVisible(!filterVisible);
+  }
+  const handleFilterSelect = (event) => {
+    setSelectedFilter(event.target.value);
+    // axios.get(`http://localhost:5000/get/users/byDept?dept=${selectedFilter}`)
+    //   .then((response) => {
+    //     setUserData(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching data through frontend..', error);
+    //   });
+    setFilterVisible(false);
+  }
 
     return(
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="center">Id</StyledTableCell>
-            <StyledTableCell align="center">Name</StyledTableCell>
-            <StyledTableCell align="center">Email</StyledTableCell>
-            <StyledTableCell align="center">Department</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {userData.map((userdata) => (
-            <StyledTableRow key={userdata.id}>
-              <StyledTableCell component="th" scope="row" align='center'>
-                {userdata.id}
+      <div>
+        <IconButton size='large' sx={{ml:140, mt:1, fontSize:20}} onClick={handleFilterIconClick}>
+          <FilterAltRoundedIcon sx={{color:'black'}}/>
+        </IconButton>
+        {filterVisible && (
+                <select style={{height:35}} value={selectedFilter} onChange={handleFilterSelect} >
+                  <option disabled>All</option>
+                  <option value="App Engg">App Engg</option>
+                  <option value="Cloud Engg">Cloud Engg</option>
+                  <option value="IOT Engg">IOT Engg</option>
+                </select>
+              )}
+        <TableContainer component={Paper} sx={{height:'490px',mt:2, width:'75%', ml:21}} >
+        <Table stickyHeader sx={{ minWidth: '75%', borderWidth:4, borderColor:'black' }} aria-label="customized table">
+          <TableHead >
+            <TableRow>
+              <StyledTableCell align="right" sx={{fontSize:'16px'}}>
+                <b>Id</b>{''}
+                <IconButton onClick={handleSort}>
+                  {sortOrder === 'asc' ? (
+                    <ArrowUpwardIcon sx={{color:'white', mt:-0.5}} />
+                  ) : (
+                    <ArrowDownwardIcon sx={{color:'white'}} />
+                  )}
+                </IconButton>
               </StyledTableCell>
-              <StyledTableCell align="center">{userdata.username}</StyledTableCell>
-              <StyledTableCell align="center">{userdata.email}</StyledTableCell>
-              <StyledTableCell align="center">{userdata.dept}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <StyledTableCell align="center" sx={{fontSize:'16px'}}><b>Name</b></StyledTableCell>
+              <StyledTableCell align="center" sx={{fontSize:'16px'}}><b>Email</b></StyledTableCell>
+              <StyledTableCell align="center" sx={{fontSize:'16px'}}><b>Department</b></StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sortedData.map((userdata) => (
+              <StyledTableRow key={userdata.id}>
+                <StyledTableCell component="th" scope="row" align='center'>
+                  {userdata.id}
+                </StyledTableCell>
+                <StyledTableCell align="center">{userdata.username}</StyledTableCell>
+                <StyledTableCell align="center">{userdata.email}</StyledTableCell>
+                <StyledTableCell align="center">{userdata.dept}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
     )
 }
 
